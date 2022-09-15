963867770f72d454bfe48d399b0ab0d09ca77c64
vadim <vadimbor93@mail.ru>
2022-09-15 11:57:11 +0300

    добавили ограничение перемещений корабля

--- ship.py
+++ ship.py
@@ -17,9 +17,9 @@ class Ship():
 
     def update(self):
         """Обновляет позицию корабля с учетом флага"""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         self.rect.x = self.x  # обновление атрибута rect на основании self.x
     
