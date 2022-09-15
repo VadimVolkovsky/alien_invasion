e8e6e7b29f088ad8a42d06dfab15d418457cd613
vadim <vadimbor93@mail.ru>
2022-09-15 13:28:32 +0300

    Добавили движение верх-вниз

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,8 +12,11 @@ class AlienInvasion:
     def __init__(self) -> None:
         pygame.init
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)) # Размер игрового окна
+        # self.screen = pygame.display.set_mode(
+        #     (self.settings.screen_width, self.settings.screen_height))  # Оконный режим
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)  # полноэкранный режим
+        self.settings.screen_width = self.screen.get_rect().width  # обновляем ширину экрана
+        self.settings.screen_height = self.screen.get_rect().height  # обновляем высоту экрана
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self.screen, self.settings)
         self.person = Person(self.screen)
@@ -31,16 +34,31 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:  # если клавиша нажата (DOWN)
-                if event.key == pygame.K_RIGHT:  # если нажата стрелка вправо
-                    self.ship.moving_right = True  # непрерывное движение вправо
-                if event.key == pygame.K_LEFT:  # если нажата стрелка влево
-                    self.ship.moving_left = True  # непрерывное движение влево
-            
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:  # если клавиша отпущена (UP)
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False  # остановка движения вправо
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False  # остановка движения влево
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:  # если нажата стрелка вправо
+            self.ship.moving_right = True  # непрерывное движение вправо
+        elif event.key == pygame.K_LEFT:  # если нажата стрелка влево
+            self.ship.moving_left = True  # непрерывное движение влево
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False  # остановка движения вправо
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False  # остановка движения влево
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
 
 
--- ship.py
+++ ship.py
@@ -12,8 +12,11 @@ class Ship():
         self.rect = self.image.get_rect()  # и получает прямоугольник.
         self.rect.midbottom = self.screen_rect.midbottom  # каждый новый корабль появляется у нижнего края экрана
         self.x = float(self.rect.x) # сохранение вещественной координаты корабля
+        self.y = float(self.rect.y)
         self.moving_right = False  # Флаг перемещения корабля вправо
         self.moving_left = False  # Флаг перемещения корабля влево
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Обновляет позицию корабля с учетом флага"""
@@ -21,7 +24,12 @@ class Ship():
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+        if self.moving_up:
+            self.y -= self.settings.ship_speed
+        if self.moving_down:
+            self.y += self.settings.ship_speed
         self.rect.x = self.x  # обновление атрибута rect на основании self.x
+        self.rect.y = self.y
     
     def blitme(self):
         """Рисует корабль в текущей позиции"""
