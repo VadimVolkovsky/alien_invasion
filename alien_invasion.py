4fa8a1142eaa724fa1569b6cfb463bbbb69343c4
vadim <vadimbor93@mail.ru>
2022-09-15 11:49:13 +0300

    отрегулировали скорость корабля до 1.5

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,7 +15,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)) # Размер игрового окна
         pygame.display.set_caption("Alien Invasion")
-        self.ship = Ship(self.screen)
+        self.ship = Ship(self.screen, self.settings)
         self.person = Person(self.screen)
 
     def run_game(self):
--- ship.py
+++ ship.py
@@ -3,24 +3,25 @@ import pygame
 
 class Ship():
     """Класс для управления кораблем"""
-    def __init__(self, ai_game):
+    def __init__(self, ai_game_screen, ai_game_settings):
         """Инициализирует корабль и задает его начальную позицию"""
-        self.screen = ai_game
-        self.screen_rect = ai_game.get_rect()
-        # Загружает изображение корабля и получает прямоугольник:
-        self.image = pygame.image.load('images/ship_1.bmp')
-        self.rect = self.image.get_rect()
-        # каждый новый корабль появляется у нижнего края экрана:
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.screen = ai_game_screen
+        self.settings = ai_game_settings
+        self.screen_rect = self.screen.get_rect()
+        self.image = pygame.image.load('images/ship_1.bmp')  # Загружает изображение корабля
+        self.rect = self.image.get_rect()  # и получает прямоугольник.
+        self.rect.midbottom = self.screen_rect.midbottom  # каждый новый корабль появляется у нижнего края экрана
+        self.x = float(self.rect.x) # сохранение вещественной координаты корабля
         self.moving_right = False  # Флаг перемещения корабля вправо
         self.moving_left = False  # Флаг перемещения корабля влево
 
     def update(self):
         """Обновляет позицию корабля с учетом флага"""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x  # обновление атрибута rect на основании self.x
     
     def blitme(self):
         """Рисует корабль в текущей позиции"""
